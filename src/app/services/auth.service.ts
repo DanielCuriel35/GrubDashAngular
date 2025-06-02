7fac7f92a6c944143158343097bccaf3ecc1daee
DanielCuriel35 <danicurielm35@gmail.com>
2025-06-02 13:14:44 +0200

    Cambio de una barra

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'https://grubdashapi-production.up.railway.app//api';
+  private apiUrl = 'https://grubdashapi-production.up.railway.app/api';
 
   constructor(private http: HttpClient) { }
 
